1cdf33d2498ee1202516a1719aa8a9b3580d9d8d
Blair Thurman <blairdev1023@gmail.com>
2018-03-25 18:30:23 -0700

    app styled

--- app.py
+++ app.py
@@ -9,10 +9,13 @@ import numpy as np
 from sklearn import datasets
 
 ################################################################################
-##################################### FUNCS ####################################
+##################################### FUNC #####################################
 ################################################################################
 
 def load_df():
+    '''
+    Returns a pandas df of the iris dataset
+    '''
     data = datasets.load_iris().data
     cols = datasets.load_iris().feature_names
     df = pd.DataFrame(data=data, columns=cols)
@@ -28,17 +31,29 @@ df = load_df()
 
 app.layout = html.Div([
     html.H2('Iris Visualization'),
-    dcc.Dropdown(
-        id='graph-dropdown-1',
-        options=[{'label': i, 'value': i} for i in df.columns],
-        value=df.columns[0]
-    ),
-    dcc.Dropdown(
-        id='graph-dropdown-2',
-        options=[{'label': i, 'value': i} for i in df.columns],
-        value=df.columns[1]
-    ),
-    dcc.Graph(id='main-graph')
+    html.Div([
+        html.Div([
+            dcc.Dropdown(
+                id='graph-dropdown-1',
+                options=[{'label': i, 'value': i} for i in df.columns],
+                value=df.columns[0]
+            )
+        ], style={'width': '50%', 'display': 'inline-block'}
+        ),
+        html.Div([
+            dcc.Dropdown(
+                id='graph-dropdown-2',
+                options=[{'label': i, 'value': i} for i in df.columns],
+                value=df.columns[1]
+            )
+        ], style={'width': '50%', 'float': 'right'}
+        ),
+        dcc.Graph(id='main-graph', config={'displayModeBar': False})
+    ],
+    style={'width': '70%',
+           'margin-left': 'auto',
+           'margin-right': 'auto'
+    })
 ])
 
 ################################################################################
@@ -54,22 +69,28 @@ def graph_maker(col1, col2):
     Returns the figure dict for main plot
     '''
 
+    color_dict = {0: 'red', 1: 'yellow', 2: 'blue'}
+
     data = []
     trace = go.Scatter(
         x=df[col1],
         y=df[col2],
         mode='markers',
-        # name=col,
-        # line={'shape': 'spline'}
+        marker={'size': 15,
+                'opacity': 0.7,
+                'color': [color_dict[i] for i in df['target']],
+                'line': {'color': 'black', 'width': 0.5}
+        }
     )
     data.append(trace)
 
     layout = go.Layout(
         xaxis={'title': col1},
         yaxis={'title': col2},
+        hovermode='closest',
+        margin={'l': 0, 'b': 0, 't': 0, 'r': 0}
     )
     return {'data': data, 'layout': layout}
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
